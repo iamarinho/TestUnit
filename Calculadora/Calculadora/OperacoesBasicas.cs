7b9d514c4dc43c2bdd1263d34ee9c2a38c3c6779
Iasmy <iasmyiasmy@gmail.com>
2017-01-02 22:40:24 -0200

    primeiro commit

---
+++ Calculadora/Calculadora/OperacoesBasicas.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Calculadora
+{
+    public class OperacoesBasicas
+    {
+        public double soma(double a, double b)
+        {
+            return (a + b);
+        }
+        public double subtracao(double a, double b)
+        {
+            return (a - b);
+        }
+        public double multiplicacao(double a, double b)
+        {
+            return (a * b);
+        }
+        public double divisao(double a, double b)
+        {
+            if (b == 0)
+            {
+                throw new DivideByZeroException("Não é permitido divisão por zero");
+            }
+            return (a / b);
+        }
+    }
+}
---
+++ Calculadora/CalculadoraTests/OperacoesBasicasTests.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculadora;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.Tests
+{
+    [TestClass()]
+    public class OperacoesBasicasTests
+    {
+        [TestMethod()]
+        public void somaTest()
+        {
+            OperacoesBasicas target = new OperacoesBasicas();
+            double x = 0;
+            double y = 0;
+            double expected = 0;
+            double actual;
+
+            actual = target.soma(x , y);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void subtracaoTest()
+        {
+            OperacoesBasicas target = new OperacoesBasicas();
+            double x = 0;
+            double y = 0;
+            double expected = 0;
+            double actual;
+
+            actual = target.subtracao(x, y);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod()]
+        public void multiplicacaoTest()
+        {
+            OperacoesBasicas target = new OperacoesBasicas();
+            double x = 0;
+            double y = 0;
+            double expected = 0;
+            double actual;
+
+            actual = target.multiplicacao(x, y);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void divisaoTest()
+        {
+            OperacoesBasicas target = new OperacoesBasicas();
+            double x = 8;
+            double y = 2;
+            double expected = 4;
+            double actual;
+
+            actual = target.divisao(x, y);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void divisaoPorZeroTest()
+        {
+            OperacoesBasicas target = new OperacoesBasicas();
+            double x = 10;
+            double y = 0;
+           
+            target.divisao(x, y);
+           
+        }
+    }
+}
